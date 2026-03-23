9fffd89895a0284582298e7eb8c3a3bc8d97dd69
vio <vioyqz.cmu@gmail.com>
2026-03-23 13:39:52 -0400

    Update download-and-run.py

--- video-script/download-and-run.py
+++ video-script/download-and-run.py
@@ -7,15 +7,14 @@ credentials, project = google.auth.default(
 
 if not credentials:
     print("错误：未能获取到 VM 身份凭证！")
-    
+
 service = build('drive', 'v3', credentials=credentials)
 
-folder_id = '1zuHPXlu3oLNYC5Ri2LQ5qA7-_vkmlCIK' 
-results = service.files().list(
-    q=f"'{folder_id}' in parents and trashed = false",
-    fields="files(id, name)").execute()
-
-items = results.get('files', [])
-print(f"found {len(items)} files：")
-for item in items:
-    print(f"- {item['name']}")
+folder_id = '1zuHPXlu3oLNYC5Ri2LQ5qA7-_vkm1CIK'
+try:
+    results = service.files().list(
+        q=f"'{folder_id}' in parents and trashed = false",
+        fields="files(id, name)").execute()
+    print(f"成功！找到 {len(results.get('files', []))} 个文件。")
+except Exception as e:
+    print(f"调用失败，错误详情: {e}")
